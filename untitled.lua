3aff0a1dc41749d101f59cdf3eafed921eb7f515
Gerard.Cristofol <user@ubuntu.ubuntu-domain>
2017-09-10 04:43:19 +1200

    compose file changes

---
+++ untitled.lua
@@ -0,0 +1,4 @@
+-- This is a one-line comment that ends at the end of the line
+--[[ This a multi-line (long) [comment](http://www.lua.org/manual/5.1/manual.html#2.1)
+     that ends with this closing bracket --> ]]
+--[=[ This is also a long comment ]=]
